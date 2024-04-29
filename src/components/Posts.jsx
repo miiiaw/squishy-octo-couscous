d8c507aa2ad00cbd7c800f63c0abf53321e718b5
miiiaw <141754937+miiiaw@users.noreply.github.com>
2024-04-29 10:56:08 +0200

    Sanity, content, scss

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,45 @@
+import { sanityClient } from "../sanity/client"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+
 export default function Posts() {
+
+    const [postData, setPost] = useState([])
+
+    //Kan flyttes til Sanity Services-mappe. Se projects-fil for annen struktur.
+    async function fetchPosts() {
+        const data = await sanityClient.fetch(`*[_type == "post"]{
+            title,
+            slug,
+            mainImage{
+                asset->{
+                    _id,
+                    url
+                },
+                alt
+            }
+    }`)
+    setPost(data)
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
     return (
         <>
-        <h1>This is the posts page!</h1>
+        <section className="postsGrid">
+            <h1>Blog posts page</h1>
+            <h2>Welcome to my page of blog posts</h2>
+                {postData.map((post, index) => (
+                <article className="postCards" key={index}>
+                    <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                        <img src={post.mainImage.asset.url} alt={post.mainImage.alt} />
+                        <h3>{post.title}</h3>
+                    </Link>
+                </article>
+                ))}
+        </section>
         </>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react"
+import { sanityClient } from "../sanity/client"
+
 export default function Projects() {
+    const [projectData, setProject] = useState([])
+
+    async function fetchProjects() {
+        const data = await sanityClient.fetch(`*[_type == "project"]{
+            title,
+            date,
+            description,
+            projectType,
+            link,
+            tags
+        }`)
+        return data
+    }
+
+    const getAllProjects = async () => {
+        const data = await fetchProjects()
+        setProject(data)
+    }
+
+    useEffect(() => {
+        getAllProjects()
+    }, [])
     return (
         <>
-        <h1>This is the projects page! Wohoo!</h1>
+        <section className="projectsGrid">
+            <h1>Projects page</h1>
+            <h2>Welcome to my projects.</h2>
+                {projectData.map((project, index) => (
+                <article className="projectCards" key={index}>
+                    <a href={project.link} alt={project.title} target="_blank">
+                        <h3>{project.title}</h3>
+                    </a>
+                    <div className="projectDetails">
+                        <span>Date: {project.date}, </span>
+                        <span>Type: {project.projectType}</span>
+                    </div>
+                    <p>
+                        {project.description}
+                    </p>
+                </article>
+                ))}
+        </section>
         </>
     )
 }
